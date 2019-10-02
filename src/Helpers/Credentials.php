2eae116e27554900ede23a2d80afa3f6e9c13c3d
emw3 <emw3wme@gmail.com>
2019-10-02 11:22:15 -0500

    [publish-files] Update config file name

--- src/Helpers/Credentials.php
+++ src/Helpers/Credentials.php
@@ -11,18 +11,18 @@ trait Credentials
     function getAllCredentials()
     {
         return [
-            'client_id' =>  config('zohocrm.client_id'),
-            'client_secret' =>  config('zohocrm.client_secret'),
-            'redirect_uri' =>  config('zohocrm.redirect_uri'),
-            'currentUserEmail' =>  config('zohocrm.current_user_email'),
-            'applicationLogFilePath' =>  config('zohocrm.application_log_file_path'),
-            'token_persistence_path' =>  config('zohocrm.token_persistence_path'),
-            'accounts_url' =>  config('zohocrm.accounts_url'),
-            'sandbox' =>  config('zohocrm.sandbox'),
-            'apiBaseUrl' =>  config('zohocrm.api_base_url'),
-            'apiVersion' =>  config('zohocrm.api_version'),
-            'access_type' =>  config('zohocrm.access_type'),
-            'persistence_handler_class' =>  config('zohocrm.persistence_handler_class'),
+            'client_id' =>  config('zoho-crm.client_id'),
+            'client_secret' =>  config('zoho-crm.client_secret'),
+            'redirect_uri' =>  config('zoho-crm.redirect_uri'),
+            'currentUserEmail' =>  config('zoho-crm.current_user_email'),
+            'applicationLogFilePath' =>  config('zoho-crm.application_log_file_path'),
+            'token_persistence_path' =>  config('zoho-crm.token_persistence_path'),
+            'accounts_url' =>  config('zoho-crm.accounts_url'),
+            'sandbox' =>  config('zoho-crm.sandbox'),
+            'apiBaseUrl' =>  config('zoho-crm.api_base_url'),
+            'apiVersion' =>  config('zoho-crm.api_version'),
+            'access_type' =>  config('zoho-crm.access_type'),
+            'persistence_handler_class' =>  config('zoho-crm.persistence_handler_class'),
         ];
     }
 }
