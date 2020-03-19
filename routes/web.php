e2319cfaa6c961ec5d7390c9c007cbc70b9d2bdf
Walter Pariona <wltrst97@gmail.com>
2020-03-18 19:15:58 -0500

    Added route to request a grant token

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
-Route::get('zoho-crm/', function () {
+Route::get('zoho-crm', function () {
     return view('interconnecta/zoho-crm::index');
+});
+
+Route::namespace('Zoho\CRM\Http\Controllers')->group(function () {
+    Route::post('zoho-crm/save', 'HomeController@store');
+    Route::get('zoho-crm/oauthredirect', 'HomeController@index');
 });
