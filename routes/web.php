bebad06e307d8dd42718118680763832c0b9e622
Walter Pariona <wltrst97@gmail.com>
2020-03-26 01:44:18 -0500

    Changed routes definition

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,8 @@
 <?php
 
-// Route::namespace('Zoho\CRM\Http\Controllers')->group(function () {
-//     Route::post('zoho-crm/save', 'HomeController@store');
-//     Route::get('zoho-crm/oauthredirect', 'HomeController@index');
-//     Route::get('zoho-crm/edit', 'HomeController@edit');
-// });
-
-Route::group([
-    'domain' => null,
-    'prefix' => config('zoho-crm.path'),
-    'namespace' => '\Zoho\CRM\Http\Controllers',
-], function () {
-    Route::get('/{view?}', 'HomeController@index')->where('view', '(.*)')->name('zoho-crm.index');
-
-    Route::prefix('api')->group(function () {
-        Route::post('/save', 'HomeController@store');
-        Route::post('/oauthredirect', 'HomeController@redirectHandler');
-    });
+Route::prefix('api')->group(function () {
+    Route::post('/save', 'HomeController@store');
+    Route::post('/oauthredirect', 'HomeController@processSecrets');
 });
+
+Route::get('/{view?}', 'HomeController@index')->where('view', '(.*)')->name('zoho-crm.index');
